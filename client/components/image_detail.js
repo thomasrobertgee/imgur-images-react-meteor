1c199f5df655f94f7c8f3e356dc1ba6f5b8def08
Thomas Gee <thomasrobertgee@gmail.com>
2017-01-31 12:04:01 +1100

    finished section 2 of the tutorial which focused mainly on React, not much on Meteor apart from the framework. Now going on to more Meteor specific learning.

--- client/components/image_detail.js
+++ client/components/image_detail.js
@@ -1,6 +1,8 @@
 // Import React
 import React from 'react';
 
+import ImageScore from './image_score';
+
 // Create our component
 const ImageDetail = (props) => {
   // props.image => this is the image object
@@ -16,6 +18,8 @@ const ImageDetail = (props) => {
         <h4 className="media-heading">
         {props.image.title}
         </h4>
+        <p>{props.image.description}</p>
+        <ImageScore ups={props.image.ups} downs={props.image.downs} />
       </div>
     </li>
   );
--- client/main.js
+++ client/main.js
@@ -1,18 +1,35 @@
 // Any JS in here is automatically ran for us
 
 // Import the React Library
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 import ImageList from './components/image_list';
 
 // Create a component
-const App = () => {
-  return (
-    <div>
-      <ImageList />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { images: [] };
+  }
+
+  componentWillMount() {
+    // Fantastic place to load data
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+      .then(response => this.setState({ images: response.data.data }));
+      // NEVER DO THIS-
+      // this.state.images = [ {}, {} ];
+  }
+
+  render() {
+    return (
+      <div>
+        <ImageList images={this.state.images}/>
+      </div>
+    );
+  }
 };
 
 // Render this component to the screen
